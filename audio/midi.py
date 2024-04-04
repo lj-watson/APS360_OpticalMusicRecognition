0f4876b8a1f805f869db52a74678464db6fb68dc
Daniel Yu <danielu6776@gmail.com>
2024-04-04 14:42:01 -0400

    Added key signature pitch changes

--- audio/midi.py
+++ audio/midi.py
@@ -6,7 +6,7 @@ from mido import MidiFile, MidiTrack, Message, MetaMessage, bpm2tempo
 import json
 import sys
 
-# List of key signatures
+# List of key signatures using Major notation which can also represent natural minors
 sharp_key_signatures = {
     0: "C",
     1: "G",
@@ -20,46 +20,107 @@ sharp_key_signatures = {
 
 flat_key_signatures = {
     0: "C",
-    1: "Dm",
-    2: "Gm",
-    3: "Cm",
-    4: "Fm",
-    5: "Bbm",
-    6: "Ebm",
-    7: "Abm"
+    1: "F",
+    2: "Bb",
+    3: "Eb",
+    4: "Ab",
+    5: "Db",
+    6: "Gb",
+    7: "Cb"
 }
 
 # Mappings to notes and durations integer equiavelent
 # TODO: fill this in
-note_mapping_gclef = {
+note_mapping = {
+    'A0': 21,
+    'A#0': 22, 'Bb0': 22,
+    'B0': 23,
+    'C1': 24,
+    'C#1': 25, 'Db1': 25,
+    'D1': 26,
+    'D#1': 27, 'Eb1': 27,
+    'E1': 28,
+    'F1': 29,
+    'F#1': 30, 'Gb1': 30,
+    'G1': 31,
+    'G#1': 32, 'Ab1': 32,
+    'A1': 33,
+    'A#1': 34, 'Bb1': 34,
+    'B1': 35,
+    'C2': 36,
+    'C#2': 37, 'Db2': 37,
+    'D2': 38,
+    'D#2': 39, 'Eb2': 39,
+    'E2': 40,
+    'F2': 41,
+    'F#2': 42, 'Gb2': 42,
+    'G2': 43,
+    'G#2': 44, 'Ab2': 44,
+    'A2': 45,
+    'A#2': 46, 'Bb2': 46,
+    'B2': 47,
+    'C3': 48,
+    'C#3': 49, 'Db3': 49,
+    'D3': 50,
+    'D#3': 51, 'Eb3': 51,
+    'E3': 52,
+    'F3': 53,
+    'F#3': 54, 'Gb3': 54,
+    'G3': 55,
+    'G#3': 56, 'Ab3': 56,
+    'A3': 57,
+    'A#3': 58, 'Bb3': 58,
+    'B3': 59,
     'C4': 60,
+    'C#4': 61, 'Db4': 61,
     'D4': 62,
+    'D#4': 63, 'Eb4': 63,
     'E4': 64,
     'F4': 65,
+    'F#4': 66, 'Gb4': 66,
     'G4': 67,
+    'G#4': 68, 'Ab4': 68,
     'A4': 69,
+    'A#4': 70, 'Bb4': 70,
     'B4': 71,
     'C5': 72,
+    'C#5': 73, 'Db5': 73,
     'D5': 74,
+    'D#5': 75, 'Eb5': 75,
     'E5': 76,
     'F5': 77,
-    'G5': 79
+    'F#5': 78, 'Gb5': 78,
+    'G5': 79,
+    'G#5': 80, 'Ab5': 80,
+    'A5': 81,
+    'A#5': 82, 'Bb5': 82,
+    'B5': 83,
+    'C6': 84,
+    'C#6': 85, 'Db6': 85,
+    'D6': 86,
+    'D#6': 87, 'Eb6': 87,
+    'E6': 88,
+    'F6': 89,
+    'F#6': 90, 'Gb6': 90,
+    'G6': 91,
+    'G#6': 92, 'Ab6': 92,
+    'A6': 93,
+    'A#6': 94, 'Bb6': 94,
+    'B6': 95,
+    'C7': 96,
+    'C#7': 97, 'Db7': 97,
+    'D7': 98,
+    'D#7': 99, 'Eb7': 99,
+    'E7': 100,
+    'F7': 101,
+    'F#7': 102, 'Gb7': 102,
+    'G7': 103,
+    'G#7': 104, 'Ab7': 104,
+    'A7': 105,
+    'A#7': 106, 'Bb7': 106,
+    'B7': 107,
+    'C8': 108
 }
-
-# note_mapping_fclef = {
-#     'E2': 60,
-#     'F2': 60,
-#     'G2': 60,
-#     'A2': 60,
-#     'B2': 60,
-#     'C3': 60,
-#     'D3': 60,
-#     'E3': 60,
-#     'F3': 60,
-#     'G3': 60,
-#     'A3': 60,
-#     'B3': 60
-# }
 
 # Duration set to 120 BPM
 duration_mapping = {
@@ -90,8 +151,10 @@ with open("symbols.json", 'r') as file:
 
 symbols = data['classification']
 
+# Sets a temporary list of symbols 
 symbols_string = symbols
 
+# Dictionary of all valid notes
 valid_notes = ["Whole_Note",
                  "Half-Note",
                  "Quarter-Note",
@@ -100,6 +163,7 @@ valid_notes = ["Whole_Note",
                  "Thirty-Two-Note",
                  "Sixty-Four-Note"]
 
+# Dictionary of all valid rests
 valid_rests = ["Whole_Rest",
                "Half-Rest",
                "Quarter-Rest",
@@ -108,13 +172,15 @@ valid_rests = ["Whole_Rest",
                "Thirty-Two-Rest",
                "Sixty-Four-Rest"]
 
+# List of all valid notes and rests respectively 
 notes = [symbol.split(":")[0] for symbol in symbols_string.strip("<>").split(":") if symbol.split(":")[0] in valid_notes]
 rests = [symbol.split(":")[0] for symbol in symbols_string.strip("<>").split(":") if symbol.split(":")[0] in valid_rests]
 
-# Make sure we have the same number of symbols and octaves
+# Separates the symbols and octave pitches
 octave_items = octaves.strip("<>").split(":")
 symbol_items = symbols.strip("<>").split(":")
 
+# Make sure we have the same number of symbols and octaves
 if len(octave_items) != len(symbol_items):
     print("Error: number of symbols and octaves do not match!")
     sys.exit()
@@ -197,6 +263,50 @@ if key == "UNKNOWN":
     print("Warning: Too many sharps/flats! Setting key signature to C")
     key = "C"
 
+# Adjusts the pitches depending on the key signature 
+adjustments = {}
+if key == "G":
+    adjustments = {'F': 'F#'}
+elif key == "D":
+    adjustments = {'F': 'F#', 'C': 'C#'}
+elif key == "A":
+    adjustments = {'F': 'F#', 'C': 'C#', 'G': 'G#'}
+elif key == "E":
+    adjustments = {'F': 'F#', 'C': 'C#', 'G': 'G#', 'D': 'D#'}
+elif key == "B":
+    adjustments = {'F': 'F#', 'C': 'C#', 'G': 'G#', 'D': 'D#', 'A': 'A#'}
+elif key == "F#":
+    adjustments = {'F': 'F#', 'C': 'C#', 'G': 'G#', 'D': 'D#', 'A': 'A#', 'E': 'F'}
+elif key == "C#":
+    adjustments = {'F': 'F#', 'C': 'C#', 'G': 'G#', 'D': 'D#', 'A': 'A#', 'E': 'F', 'B': 'C'}
+elif key == "F":
+    adjustments = {'B': "Bb"}
+elif key == "Bb":
+    adjustments = {'B': "Bb", 'E': 'Eb'}
+elif key == "Eb":
+    adjustments = {'B': "Bb", 'E': 'Eb', 'A': 'Ab'}
+elif key == "Ab":
+    adjustments = {'B': "Bb", 'E': 'Eb', 'A': 'Ab', 'D': 'Db'}
+elif key == "Db":
+    adjustments = {'B': "Bb", 'E': 'Eb', 'A': 'Ab', 'D': 'Db', 'G': 'Gb'}
+elif key == "Gb":
+    adjustments = {'B': "Bb", 'E': 'Eb', 'A': 'Ab', 'D': 'Db', 'G': 'Gb', 'C': 'B'}
+elif key == "Cb":
+    adjustments = {'B': "Bb", 'E': 'Eb', 'A': 'Ab', 'D': 'Db', 'G': 'Gb', 'C': 'B', 'F': 'E'}
+
+# Makes the adjustment for all key signatures other than C-Major or A-minor
+pitch_items = []
+if key is not "C":
+    for note in octave_items:
+        root_note = note[0]
+        if root_note in adjustments:
+            adjusted_note = adjustments[root_note] + note[1:]
+        else:
+            adjusted_note = note
+        pitch_items.append(adjusted_note)
+else:
+    pitch_items = octave_items
+
 # Finally append key
 track.append(MetaMessage("key_signature", key=key))
 
@@ -205,9 +315,9 @@ track.append(MetaMessage("key_signature", key=key))
 cumulative_ticks = 0
 
 # Process notes, rests, and durations
-for note, duration_sym in zip(octave_items, symbol_items):
-    # Get note and duration from mapping dictionaries 
-    midi_note = note_mapping_gclef.get(note, None)
+for note, duration_sym in zip(pitch_items, symbol_items):
+    # Get note and duration from mapping dictionaries         
+    midi_note = note_mapping.get(note, None)
     ticks = duration_mapping.get(duration_sym, None)
 
     # Checks if both notes and duration are valid
@@ -223,4 +333,4 @@ for note, duration_sym in zip(octave_items, symbol_items):
         print(f"Not a note: {note}, {duration_sym}")
 
 track.append(MetaMessage('end_of_track'))
-mid.save("piano_track.mid")
+mid.save("score.mid")
